974efb783d62f8f4e29e8601c6740fddf613cc43
AdityaVenkatasundaram <85490617+AdityaVenkatasundaram@users.noreply.github.com>
2023-03-10 16:28:10 +1100

    Week 1 Practical T1
    executed a simple program using html javascript

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function changeText() {
+    var textsArray = ["SIT709", "SIT726", "SIT716", "SIT317", "SIT778"];
+    var number = getRandomNumberBetween(0,textsArray.length - 1);
+
+    console.log("Index: ", number);
+    document.getElementById("heading").innerHTML = textsArray[number];
+}
+
+
+
+function getRandomNumberBetween(min,max){
+
+    return Math.floor(Math.random()*(max-min+1)+min);
+
+}
